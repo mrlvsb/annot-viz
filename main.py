ebceb3cce644d4832c462fba66e474ea7e19e359
Jan Gaura <jan.gaura@gmail.com>
2020-02-18 13:52:50 +0100

    Displaying next or prev image

--- main.py
+++ main.py
@@ -87,8 +87,9 @@ class Viz:
         self.h = 500
 
         self.display = pygame.display.set_mode((self.w, self.h), pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.RESIZABLE)
-        pygame.display.set_caption("Example resizable window")
-        timeline_np_img, self.image = preprocess(*sys.argv[1:])
+        pygame.display.set_caption("Annot Viz")
+        timeline_np_img, self.image, self.image_filenames = preprocess(*sys.argv[1:])
+        self.current_image_index = 0
 
         self.timeline_np_img = timeline_np_img.transpose(1, 0, 2)
         self.timeline_image = pygame.surfarray.make_surface(self.timeline_np_img)
@@ -103,6 +104,24 @@ class Viz:
     #def on_root_resize(self, event):
 
     #def foo(self, annot_timeline_img : Image, image : Image):
+
+    def display_next(self, amount=1):
+        if len(self.image_filenames) > self.current_image_index + amount:
+            self.current_image_index = self.current_image_index + amount
+        else:
+            self.current_image_index = 0
+
+        self.image = pygame.image.load(os.path.join(sys.argv[2], self.image_filenames[self.current_image_index]))
+
+
+    def display_prev(self, amount=1):
+        if self.current_image_index - amount > 0:
+            self.current_image_index = self.current_image_index - amount
+        else:
+            self.current_image_index = len(self.image_filenames) - amount
+
+        self.image = pygame.image.load(os.path.join(sys.argv[2], self.image_filenames[self.current_image_index]))
+
 
     def run(self):
         while True:
@@ -119,15 +138,28 @@ class Viz:
 
             pygame.display.update()
 
-            pygame.display.update()
             for event in pygame.event.get():
                 if event.type == pygame.MOUSEBUTTONDOWN:
+                    button_l, button_m, button_r = pygame.mouse.get_pressed()
                     pos = pygame.mouse.get_pos()
-                    print('mouse:', pos)
-                    #clicked_sprites = [s for s in [self.timeline_sprite] if s.rect.collidepoint(pos)]
-                    #for s in clicked_sprites:
-                    #    print(s)
-
+                    #print('mouse:', pos)
+                    #print(type(self.timeline_sprite))
+                    #print(len(self.timeline_sprite))
+                    #print(self.timeline_sprite.sprites())
+                    clicked_sprites = [s for s in [*self.timeline_sprite.sprites(), *self.image_sprite.sprites()] if s.rect.collidepoint(pos)]
+                    for s in clicked_sprites:
+                        if isinstance(s, ImageSprite):
+                            pos_x, pos_y = pos
+                            if pos_x > self.w / 2:
+                                if button_l:
+                                    self.display_next()
+                                elif button_r:
+                                    self.display_next(24)
+                            else:
+                                if button_l:
+                                    self.display_prev()
+                                elif button_r:
+                                    self.display_prev(24)
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
@@ -135,8 +167,16 @@ class Viz:
                     if event.key == pygame.K_ESCAPE:
                         pygame.quit()
                         sys.exit()
-                    if event.key == pygame.K_r:
+                    elif event.key == pygame.K_r:
                         pass
+                    elif event.key == pygame.K_RIGHT:
+                        self.display_next()
+                    elif event.key == pygame.K_PAGEUP:
+                        self.display_next(24)
+                    elif event.key == pygame.K_LEFT:
+                        self.display_prev()
+                    elif event.key == pygame.K_PAGEDOWN:
+                        self.display_prev(24)
                         #print(pygame.display.get_window_size())
                         #print('h: {}, w: {}'.format(pygame.display.get_height()), pygame.display.get_width())
 
@@ -204,12 +244,12 @@ def preprocess(annot_filename, imgs_dir):
 
         imageio.imwrite('annot.png', annot_img)
 
-    return annot_img, img
+    return annot_img, img, ls
 
 
 def main():
     if len(sys.argv) == 3:
-        timeline_img, image = preprocess(*sys.argv[1:])
+        #timeline_img, image = preprocess(*sys.argv[1:])
         viz = Viz()
 
         viz.run()
